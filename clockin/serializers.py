40a0135443cfc8e0d95eda474943b3ab821d3ccc
Ryan Dines <ryan.dines@gmail.com>
2018-03-06 08:47:11 -0500

    Added serializer.  Added REST endpoints.

---
+++ clockin/serializers.py
@@ -0,0 +1,7 @@
+from .models import IntervalWork
+from rest_framework import serializers
+
+class IntervalWorkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IntervalWork
+        fields = ('user', 'started', 'finished', 'comments')
