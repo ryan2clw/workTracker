7ce8e189ef942f61eda5109ae612f636ec699ec5
Ryan Dines <ryan.dines@gmail.com>
2018-04-04 01:36:04 -0400

    Added static content to developer section.  Fixed hard-coded url for CustomLoginView.

--- login/views.py
+++ login/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.views import LoginView
+from django.shortcuts import reverse
+from invoice.views import InvoiceView
         
 class CustomLoginview(LoginView):
     
     def get_redirect_url(self):
         if self.request.user.groups.filter(name="customer").exists():
-            return 'invoice'
+            return reverse('bill')
+            #return 'invoice'
         return super().get_redirect_url()
