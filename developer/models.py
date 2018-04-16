4985ee5642190833d511e4c21996cd37b9337f60
Ryan Dines <ryan.dines@gmail.com>
2018-04-15 23:37:44 -0400

    added mail.

--- developer/models.py
+++ developer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    created_on = models.DateField(auto_now_add=True)
+    message = models.TextField(null=True)
+    
+    def __str__(self):
+        return self.name + ", " + self.phone + ", " + self.email + ", " + self.message
--- developer/views.py
+++ developer/views.py
@@ -1,5 +1,9 @@
 #from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from developer.forms import ContactForm
+from django.shortcuts import reverse
+import boto3
 
 class DeveloperView(TemplateView):
     template_name = 'developer/index.html'
@@ -10,8 +14,42 @@ class AboutView(TemplateView):
 class SampleView(TemplateView):
     template_name = 'developer/post.html'
     
-class ContactView(TemplateView):
-    template_name = 'developer/contact.html'
+class ContactView(FormView):
 
+    form_class = ContactForm
+    success_url = 'sendmail'
+    template_name = 'developer/contact.html'
+    client = boto3.client('ses')
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        self.object = form.save()
+        response = client.send_email(
+            Source='string',
+            Destination={
+                'ToAddresses': [
+                    'ryan.dines@gmail.com',
+                ],
+            },
+            Message={
+                'Subject': {
+                    'Data': 'hi',
+                },
+                'Body': {
+                    'Text': {
+                        'Data': 'hi',
+                    },
+                    'Html': {
+                        'Data': '<h3>HUGE</h3>',
+                    }
+                }
+            },
+            ReplyToAddresses=[
+                'info@seniordevops.com',
+            ]
+        )
+        return super().form_valid(form)
     
+class EmailView(TemplateView):
+    template_name = 'developer/sendEmail.html'    
 
