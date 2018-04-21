187803b47d7d59f2f32671a3929fc205632e8372
Ryan Dines <ryan.dines@gmail.com>
2018-04-21 09:51:48 -0400

    Added Rest endpoint for mobile app.

--- clockin/views.py
+++ clockin/views.py
@@ -1,37 +1,34 @@
+import logging
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from rest_framework.generics import UpdateAPIView, CreateAPIView
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+from django.shortcuts import reverse
+from pytz import timezone as pytzTZ
+from rest_framework.generics import UpdateAPIView, CreateAPIView, ListAPIView
 from django_tables2 import RequestConfig
 from clockin.models import IntervalWork
 from clockin.serializers import IntervalWorkSerializer
-from django.utils import timezone
-from pytz import timezone as pytzTZ
 from clockin.forms import ClockinForm
 from clockin.tables import IntervalTable
 from invoice.models import Project
-from django.http import HttpResponseRedirect
-from django.shortcuts import reverse
-import logging
 
-log = logging.getLogger("workTracker")
-        
+LOG = logging.getLogger("workTracker")
+
 # Clockin View
 
 class IndexView(LoginRequiredMixin, ListView):
     model = IntervalWork
     template_name = 'clockin/index.html'
-    context_object_name = 'interval' # the variable name of the object in the template
+    context_object_name = 'IntervalWork' # the variable name of the object in the template
     ordering = ['id']
     myProjects = None
     currentProject = None
-    
     def get_queryset(self):
-        # Checks if they're signed in somewhere, grabs those objects, otherwise uses GET parameter to filter work objects,
-        # otherwise, returns none, user hasn't chosen a project nor is he signed into one.
         self.myProjects = Project.objects.all()
         for project in self.myProjects:
-            IntervalWorks = IntervalWork.objects.filter(user_id=self.request.user.id, project_id=project.id,
-                started__gte=timezone.now().astimezone(pytzTZ('US/Eastern')).replace(hour=0, minute=0, second=0)).values()
+            IntervalWorks = IntervalWork.objects.filter(user_id=self.request.user.id, project_id=project.id, 
+            started__gte=timezone.now().astimezone(pytzTZ('US/Eastern')).replace(hour=0, minute=0, second=0)).values()
             for work in IntervalWorks:
                 if not work["finished"]:
                     self.currentProject = project.name
@@ -86,3 +83,30 @@ class WorkUpdate(UpdateAPIView):
 class WorkCreate(CreateAPIView):
     queryset = IntervalWork.objects.all()
     serializer_class = IntervalWorkSerializer
+
+class WorkList(ListAPIView):
+    queryset = IntervalWork.objects.all()
+    serializer_class = IntervalWorkSerializer
+
+    myProjects = None
+    currentProject = None
+    
+    def get_queryset(self):
+        self.myProjects = Project.objects.all()
+        for project in self.myProjects:
+            IntervalWorks = IntervalWork.objects.filter(user_id=self.request.user.id, project_id=project.id, 
+            started__gte=timezone.now().astimezone(pytzTZ('US/Eastern')).replace(hour=0, minute=0, second=0)).values()
+            for work in IntervalWorks:
+                if not work["finished"]:
+                    self.currentProject = project.name
+        if self.currentProject:
+            myProject = Project.objects.get(name=self.currentProject)
+            return IntervalWork.objects.filter(user_id=self.request.user.id, project_id=myProject.id,
+                started__gte=timezone.now().astimezone(pytzTZ('US/Eastern')).replace(hour=0, minute=0, second=0)).order_by('started')
+        else:
+            try:
+                myProject = Project.objects.get(name=self.request.GET["project"])
+                return IntervalWork.objects.filter(user_id=self.request.user.id, project_id=myProject.id,
+                    started__gte=timezone.now().astimezone(pytzTZ('US/Eastern')).replace(hour=0, minute=0, second=0)).order_by('started')
+            except:
+                return IntervalWork.objects.none()
