b08c80265f86fb70332849ad4a3dd06b2712d2d3
Ryan Dines <ryan.dines@gmail.com>
2018-06-26 20:44:53 -0400

    Fixed url routing bug for activation bug.

--- register/views.py
+++ register/views.py
@@ -1,106 +1,48 @@
-from django.shortcuts import render
-from django.views.generic.edit import FormView
-from register.forms import RegistrationFormUniqueEmail
-from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import render, redirect
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode
+from django.template.loader import render_to_string
+from register.forms import RegistrationForm
+from register.tokens import account_activation_token
+from django.contrib.auth import login
 from django.contrib.auth.models import User
-from django.shortcuts import redirect
-import requests, json
+import boto3
 
-# Create your views here.
-class RegistrationView(FormView):
-    """
-    Base class for user registration views.
-    """
-    disallowed_url = 'registration_disallowed'
-    form_class = RegistrationFormUniqueEmail
-    success_url = 'menu'
-    template_name = 'register'
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
 
-    def dispatch(self, *args, **kwargs):
-        """
-        Check that user signup is allowed before even bothering to
-        dispatch or do other processing.
-        """
-        if not self.registration_allowed():
-            return redirect(self.disallowed_url)
-        return super(RegistrationView, self).dispatch(*args, **kwargs)
-
-    def form_valid(self, form):
-        ''' Begin reCAPTCHA validation '''
-        recaptcha_response = self.request.POST.get('g-recaptcha-response')
-        url = 'https://www.google.com/recaptcha/api/siteverify'
-        values = {
-            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-            'response': recaptcha_response
-        }
-        response = requests.post(url, values)
-        ''' End reCAPTCHA validation '''
-
-        if response.ok:
-            new_user = self.register(form)
-            success_url = self.success_url
-            try:
-                to, args, kwargs = success_url
-                return redirect(to, *args, **kwargs)
-            except ValueError:
-                return redirect(success_url)
-        else:
-            messages.error(request, 'Invalid reCAPTCHA. Please try again.')    
-            
-            
-    def registration_allowed(self):
-        """
-        Override this to enable/disable user registration, either
-        globally or on a per-request basis.
-        """
-        return getattr(settings, 'REGISTRATION_OPEN', True)
-
-    def register(self, form):
-        user = User.objects.create(username=self.request.POST['username'], password=form.fields['password1'])
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.profile.email_confirmed = True
         user.save()
-        return
-        """
-        Implement user-registration logic here. Access to both the
-        request and the registration form is available here.
-        """
-        #raise NotImplementedError
+        login(request, user)
+        return redirect('project') # Send user to make his 1st project or back to login
+    else:
+        return redirect('login') # MARK TO DO: ADD 403 ERROR MESSAGE TO LOGIN ?message=noauth
 
-'''
-class ActivationView(TemplateView):
-    """
-    Base class for user activation views.
-    """
-    success_url = None
-    template_name = 'register.html'
-
-    def get(self, *args, **kwargs):
-        """
-        The base activation logic; subclasses should leave this method
-        alone and implement activate(), which is called from this
-        method.
-        """
-        activated_user = self.activate(*args, **kwargs)
-        if activated_user:
-            signals.user_activated.send(
-                sender=self.__class__,
-                user=activated_user,
-                request=self.request
-            )
-            success_url = self.get_success_url(activated_user) if \
-                (hasattr(self, 'get_success_url') and
-                 callable(self.get_success_url)) else \
-                self.success_url
-            try:
-                to, args, kwargs = success_url
-                return redirect(to, *args, **kwargs)
-            except ValueError:
-                return redirect(success_url)
-        return super(ActivationView, self).get(*args, **kwargs)
-
-    def activate(self, *args, **kwargs):
-        """
-        Implement account-activation logic here.
-        """
-        raise NotImplementedError
-        
-'''
+def signup(request):
+    client = boto3.client('ses', region_name='us-east-1')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate Your WorkTracker Account'
+            message = render_to_string('account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject, message)
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
